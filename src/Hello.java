35a0cbe6257649c484c006db07d90694a80429be
Miley Kim <issolar21@gmail.com>
2024-01-15 14:40:17 +0900

    세번째 커밋

--- src/Hello.java
+++ src/Hello.java
@@ -14,13 +14,16 @@ public class Hello {
 		
 		System.out.println(num3);
 		
-		//System.out.println("Hello");
-		//System.out.println("Programmer");
-		//System.out.println("È«±æµ¿");
-		//System.out.println("È«±æ¼ø");
+		System.out.println("Hello");
+		System.out.println("Programmer");
+		System.out.println("È«±æµ¿");
+		System.out.println("È«±æ¼ø");
 		
-		//System.out.println(1+2);
-		//System.out.println(3+5);
+		System.out.println(1+2);
+		System.out.println(3+5);
+		
+		System.out.println("¾È³çÇÏ¼¼¿ä");
+		
 		
 	}
 
